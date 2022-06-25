a5efc6993a68ef5c9717763fce602c52b361e6a8
Gulnara <gulnara@Gulnara-MacBook-Pro.local>
2022-06-25 13:53:02 +0300

    added sheep counter

---
+++ triangle.js
@@ -0,0 +1,10 @@
+function isTriangle(a,b,c) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      return false;
+    } else if (a+b > c && a+c > b && c+b > a){
+      return true;
+    } else {
+      return false;
+    }    
+         
+}
