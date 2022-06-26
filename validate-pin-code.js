1d47b50000ae213be9dd6ca40a49a6766ac2a7d1
Gulnara <gulnara@Gulnara-MacBook-Pro.local>
2022-06-26 22:20:26 +0300

    hashtag generator

--- validate-pin-code.js
+++ validate-pin-code.js
@@ -1,11 +1,12 @@
 function validatePIN (pin) {
     const checkLength = pin.length === 4 || pin.length === 6;
-  
-    const numMatch = /^[0-9]/g
-    
-    if (checkLength && pin.match(numMatch)){  
-      return true;
-    }  
-    
-    return false;
-    }
+   
+   const numMatch = /^[0-9]/g
+   var er = /^-?[0-9]+$/;
+   if(er.test(pin)){
+   if (checkLength && pin.match(numMatch)){
+   return true;
+   }
+   }
+   return false;
+     }
